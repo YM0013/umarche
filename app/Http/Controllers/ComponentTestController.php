0005fa573634c878e5e4163ad8408ef3c62826ef
YM0013 <kaziki0013@gmail.com>
2024-09-17 16:08:05 +0900

    component準備

---
+++ app/Http/Controllers/ComponentTestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComponentTestController extends Controller
+{
+    public function showComponettest1()
+    {
+        return view('tests.component-test1');
+    }
+
+    public function showComponettest2()
+    {
+        return view('tests.component-test2');
+    }
+}
