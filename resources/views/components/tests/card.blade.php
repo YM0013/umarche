4c2fff9ed72a09475c68bdef91292d68b28736e1
YM0013 <kaziki0013@gmail.com>
2024-10-25 14:13:39 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'title' => '初期値タイトルです。',
+    'message' => '初期値です。',
+    'content' => '初期値本文です。'
+])
+
 <div class = "border-2 shasow-md w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
