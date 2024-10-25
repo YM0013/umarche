f21f147aeff2591354c8b43da922b217956dfb34
YM0013 <kaziki0013@gmail.com>
2024-10-25 13:58:28 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,7 +8,11 @@ class ComponentTestController extends Controller
 {
     public function showComponettest1()
     {
-        return view('tests.component-test1');
+        $message = 'メッセージ123';
+        return view(
+            'tests.component-test1',
+            compact('message')
+        );
     }
 
     public function showComponettest2()
