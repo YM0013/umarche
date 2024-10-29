4307b923e54fe87732ff9b4a9a2bd602a30f55bc
YM0013 <kaziki0013@gmail.com>
2024-10-29 13:37:57 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,9 @@
     'content' => '初期値本文です。'
 ])
 
-<div class = "border-2 shasow-md w-1/4 p-2">
+<div {{ $attributes ->merge([
+    'class' => 'border-2 shasow-md w-1/4 p-2'
+   ]) }} >
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
