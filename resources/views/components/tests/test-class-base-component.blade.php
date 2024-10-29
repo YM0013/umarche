daf5e0170acdb22d0c8af2f4ca96abf4c796f92a
YM0013 <kaziki0013@gmail.com>
2024-10-29 14:25:59 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントテスト
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,6 +1,10 @@
 <x-tests.app>
-<x-slot neme="header">ヘッダー２</x-slot>
+  <x-slot name="header">ヘッダー２</x-slot>
 コンポーネント２
 
+<x-test-class-base classBaseMessage="メッセージです" />
+
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 <x-tests.card title="タイトル1" content="本文" />
 </x-tests.app>
