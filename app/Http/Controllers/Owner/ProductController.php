dc978a8d995bdec1a03711071cfabff3b48fd808
YM0013 <kaziki0013@gmail.com>
2024-12-15 04:18:41 +0900

    product update

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -167,6 +167,59 @@ class ProductController extends Controller
         $request->validate([
             'current_quantity' => ['required', 'integer',],
         ]);
+
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity'); //一つの商品の在庫を取ることになるのでStockになっている
+
+        if ($request->current_quantity !== strval($quantity)) {
+            $id = $request->route()->parameter('product');
+            return redirect()->route('owner.products.edit', ['product' => $id])
+                ->with([
+                    'message' => '在庫数が変更されています。再度確認してください。',
+                    'status' => 'alert'
+                ]);
+        } else {
+
+            try {
+                DB::transaction(function () use ($request, $product) {
+
+                    $product->name = $request->name;
+                    $product->information = $request->information;
+                    $product->price = $request->price;
+                    $product->sort_order = $request->sort_order;
+                    $product->shop_id = $request->shop_id;
+                    $product->secondary_category_id = $request->category;
+                    $product->image1 = $request->image1;
+                    $product->image2 = $request->image2;
+                    $product->image3 = $request->image3;
+                    $product->image4  = $request->image4;
+                    $product->is_selling = $request->is_selling;
+                    $product->save();
+
+                    if ($request->type === '1') {
+                        $newQuantity = $request->quantity;
+                    }
+                    if ($request->type === '2') {
+                        $newQuantity = $request->quantity * -1;
+                    }
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity,
+                    ]);
+                }, 2);
+            } catch (Throwable $e) {
+                Log::error($e);
+                throw ($e);
+            }
+
+            return redirect()
+                ->route('owner.products.index')
+                ->with([
+                    'message' => '商品情報を更新しました。',
+                    'status' => 'info'
+                ]);
+        }
     }
 
     /**
--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -10,6 +10,7 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />    
+                    <x-flash-message status="session('status')" /> 
                     <form method="post" action="{{route('owner.products.update',['product'=> $product->id])}}" >
                         @csrf
                         @method('put')
@@ -37,7 +38,7 @@
                                 <div class="p-2 w-1/2 mx-auto">
                                   <div class="relative">
                                     <label for="sort_order" class="leading-7 text-sm text-gray-600">表示順</label>
-                                    <input type="number" id="sort_order" name="sort_order" value="{{ $product->sore_order }}"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="number" id="sort_order" name="sort_order" value="{{ $product->sort_order }}"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                   </div>
                                 </div>
                             </div>
