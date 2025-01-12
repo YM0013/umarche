38ed45e954cc071d2208d283971d91fb1105b3a6
YM0013 <kaziki0013@gmail.com>
2025-01-13 00:00:23 +0900

    検索フォーム　カテゴリー

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -27,11 +27,11 @@
                 <p class="text-sm text-gray-700 leading-5">
                     
                     
-                    <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                    件中
-                    <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                    件～
                     <span class="font-medium">{{ $paginator->total() }}</span>
+                    件中
+                    <span class="font-medium">{{ $paginator->firstItem() }}</span>
+                    件～
+                    <span class="font-medium">{{ $paginator->lastItem() }}</span>
                     件 を表示
                 </p>
             </div>
